f532e827b99da83be99f5b75de6bc23832f4c73b
Beatriz Geirinhas <bgeirinhas@dodoc.com>
2022-02-12 02:04:40 +0000

    fixing build error 2

--- stitches.config.js
+++ stitches.config.js
@@ -45,17 +45,6 @@ export const {
       8: "65px",
       9: "80px",
     },
-    space: {
-      1: "5px",
-      2: "10px",
-      3: "15px",
-      4: "20px",
-      5: "25px",
-      6: "35px",
-      7: "45px",
-      8: "65px",
-      9: "80px",
-    },
   },
   media: {
     bp1: "(min-width: 520px)",
